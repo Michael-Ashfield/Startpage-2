922781417fb09435dab22e9cdf13f0f4a55ec9e8
Michael Ashfield <2ashfm61@solent.ac.uk>
2017-02-12 22:50:13 +0000

    Rework, reduced CSS, added JS file
    JS
    -Moved all scripts into mainjs.js
    -Remade clock, made it 12 hour
    
    CSS
    -Made all menus run off of one style, cut CSS by about half
    -Cleaned the menu styling and removed unneeded lines
    -Using CSS variables for all colors, makes color changing easy
    -Removed random right padding on mainbar
    
    HTML
    -Only modified to accomidated the CSS and JS changes

---
+++ Startpage2 GeneralEdition/mainjs.js
@@ -0,0 +1,29 @@
+##12 Hour Clock script##
+function startTime(){
+    var today = new Date();
+    var curr_hour = today.getHours();
+    var curr_minute = today.getMinutes();
+    var curr_second = today.getSeconds();
+    curr_second = checkTime(curr_second);
+    curr_minute = checkTime(curr_minute);
+    curr_hour = checkTime(curr_hour);
+    var curr_time = convertTime(curr_hour) +":"+ curr_minute +":"+ curr_second;
+    console.log(curr_time);
+    document.getElementById("time").innerHTML = curr_time;  
+}
+
+var myTimer = setInterval(startTime, 500);
+
+function convertTime(curr_hour){
+    if (curr_hour >= 12){
+        return(curr_hour-12);
+    }
+    else {
+        return(curr_hour);
+    }
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
